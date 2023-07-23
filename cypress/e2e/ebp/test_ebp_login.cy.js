4c03093e20d518458b48bf86e617ff33056fa5e5
libbywu2023 <133045194+libbywu2023@users.noreply.github.com>
2023-07-22 23:48:36 -0700

    update testcase -login

--- cypress/e2e/ebp/test_ebp_login.cy.js
+++ cypress/e2e/ebp/test_ebp_login.cy.js
@@ -1,6 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://ebp-test.ebuilding.ca/')
-    cy.contains('Log In') //123
+describe('EBP-UI-login', () => {
+  it('contributor logs in and verifies success page', () => {
+    cy.visit('https://ebp-test.ebuilding.ca/');
+    //verifies 1
+    cy.contains('Log In');
+
+    //<input class="……" name="email" type="email" placeholder="Email" value="">
+    cy.get('[name="email"]').type('testcontributor@ebuilding.ca');
+    //<input class="……" name="password" type="password" placeholder="Password" value="">
+    cy.get('[name="password"]').type('Test@1234');
+
+    //<button type="submit" class="……">Log In</button>
+    cy.get('button[type="submit"]').click();
+
+    cy.wait(4000);
+    //verifies 2
+    cy.contains('Dashboard');
+    //cy.contains('Sign out');
+
   })
 })

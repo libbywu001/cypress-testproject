0bae10a1cccc70dc4d65ea53fef9ef167cd1560e
libbywu2023 <133045194+libbywu2023@users.noreply.github.com>
2023-07-24 11:16:37 -0700

    bugfix

--- cypress/e2e/ebp/test_ebp_login.cy.js
+++ cypress/e2e/ebp/test_ebp_login.cy.js
@@ -1,8 +1,8 @@
 describe('EBP-UI-login', () => {
   it('contributor logs in and verifies success page', () => {
     cy.visit('https://ebp-test.ebuilding.ca/');
-    //verifies 1
-    cy.contains('Log In');
+    //cy.contains('Log In');
+    cy.contains('Log In', { timeout: 4000 }).should('be.visible');
 
     //<input class="……" name="email" type="email" placeholder="Email" value="">
     cy.get('[name="email"]').type('testcontributor@ebuilding.ca');
@@ -12,10 +12,14 @@ describe('EBP-UI-login', () => {
     //<button type="submit" class="……">Log In</button>
     cy.get('button[type="submit"]').click();
 
-    cy.wait(4000);
-    //verifies 2
-    cy.contains('Dashboard');
-    //cy.contains('Sign out');
+    //cy.wait(4000);
+    cy.contains('Dashboard', { timeout: 8000 }).should('exist');
+    cy.contains('Sign out', { timeout: 4000 }).should('exist');
 
+    cy.log('now sign out');
+    cy.contains('Sign out').click();
+
+    //
+    cy.contains('Log In', { timeout: 4000 }).should('exist');
   })
 })
